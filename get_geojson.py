f12ea707ee6323c969f509cfa880f3a538d8f47d
Tom Holderness <tom.holderness@ncl.ac.uk>
2012-12-04 23:08:27 +0000

    Broken get_geojson - need to fix geom conversions from BBox WKT.

--- get_geojson.py
+++ get_geojson.py
@@ -35,7 +35,7 @@ conn = psycopg2.connect("dbname=ukmap_london user=postgres password=postgres")
 cur = conn.cursor()
 
 # Query
-cur.execute('SELECT row_to_json(fc) FROM ( SELECT \'FeatureCollection\' As type, array_to_json(array_agg(f)) As features FROM (SELECT \'Feature\' As type, ST_AsGeoJSON(ST_Transform(lg.the_geom,4326))::json As geometry, row_to_json((SELECT l FROM (SELECT height) As l)) As properties FROM "3DLondon" As lg WHERE ST_Within(lg.the_geom, ST_Transform(ST_GeomFromText(%s,4326),27700)) LIMIT 100000 ) As f )  As fc;',[bbox_wkt])
+cur.execute('SELECT row_to_json(fc) FROM ( SELECT \'FeatureCollection\' As type, array_to_json(array_agg(f)) As features FROM (SELECT \'Feature\' As type, ST_AsGeoJSON(ST_Transform(lg.geom,4326))::json As geometry, row_to_json((SELECT l FROM (SELECT height) As l)) As properties FROM "3DLondonBuildings" As lg WHERE ST_Within(lg.geom, ST_GeomFromText(%s,4326)) LIMIT 100000 ) As f )  As fc;',[bbox_wkt])
 array = cur.fetchall()
 
 # Return data as GeoJSON
